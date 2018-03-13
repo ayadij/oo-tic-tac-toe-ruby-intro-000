df5a2961131ad76d41151ee94472ee6a5a58e114
ayadij <ayadijkwel@yahoo.com>
2018-03-12 22:06:49 -0600

    finished adding comments to methods

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,8 @@ class TicTacToe
     end
   end
 
-  # return false/nil
+  # return false/nil if there are no wins
+  # returns the winning combination indexes as an array if there is a win
   def won?
     win_index_0 = WIN_COMBINATIONS[0]
     win_index_1 = WIN_COMBINATIONS[1]
@@ -118,18 +119,23 @@ class TicTacToe
     end
   end
 
+  # return true if every element in the board contains either an "X" or an "O"
   def full?
     @board.none?{|element|element==" "}
   end
 
+  # returns true if the board is full and has no wins
+  # returns false otherwise
   def draw?
     !won? && full?
   end
 
+  # returns true if the game is over, ie, if the board has been won or is full 
   def over?
     won? || draw?
   end
 
+  # returns who won the game
   def winner
     if !!won? && @board[won?[0]] == "X"
       "X"
@@ -140,58 +146,20 @@ class TicTacToe
     end
   end
 
+
+
+  # main method of the game and responsible for the loop.
   def play
-    until over?
-      turn
+    until over?       # until the game is over,
+      turn              # take turns
     end
-    if won?
-      puts "Congratulations #{winner}!"
-    else
-      puts "Cat's Game!"
+    if won?           # if the game is won,
+      puts "Congratulations #{winner}!"   # puts
+    else              # else if the game was a draw
+      puts "The game ended in a draw!"    # puts
     end
   end
 
 end
 
-# https://github.com/ayadij/tic-tac-toe
-
-
-
-
-        # #won?
-        # Your #won? method should return false/nil if there is no win combination present in the board and return the winning combination indexes as an array if there is a win. Use your WIN_COMBINATIONS constant in this method.
-
-        # #full?
-        # The #full? method should return true if every element in the board contains either an "X" or an "O".
-
-        # #draw?
-        # Build a method #draw? that returns true if the board is full and has not been won, false if the board is won, and false if the board is neither won nor full.
-
-        # #over?
-        # Build a method #over? that returns true if the board has been won or is full (i.e., is a draw).
-
-        # #winner
-        # Given a winning @board, the #winner method should return the token, "X" or "O", that has won the game.
-
-        # Putting it all together: the #play method
-        # #play
-        # The play method is the main method of the Tic Tac Toe application and is responsible for the game loop. A Tic Tac Toe game must allow players to take turns, checking if the game is over after every turn. At the conclusion of the game, whether because it was won or ended in a draw, the game should report to the user the outcome of the game. You can imagine the pseudocode:
-
-        # until the game is over
-        #   take turns
-        # end
-
-        # if the game was won
-        #   congratulate the winner
-        # else if the game was a draw
-        #   tell the players it ended in a draw
-        # end
-        # Run the tests for the #play method by typing rspec spec/02_play_spec.rb in your terminal.
-
-        # The CLI: bin/tictactoe
-        # Your bin/tictactoe CLI should:
-
-        # Instantiate an instance of TicTacToe
-        # Start the game by calling #play on that instance.
-        # View OO Tic Tac Toe on Learn.co and start learning to code for free.
-
+# https://github.com/ayadij/tic-tac-toe 
